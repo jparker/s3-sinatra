8b0d0d16b2565c1aa3a62fd6119feccbdfd99221
John Parker <jparker@urgetopunt.com>
2010-03-19 14:55:15 -0700

    Added success notification, ajax addition of upload fields, vaguely more RESTful routes

--- application.rb
+++ application.rb
@@ -12,7 +12,7 @@ ENV['RACK_ENV'] ||= 'development'
 #   :access_key_id     => AWS access key
 #   :secret_access_key => AWS secret access key
 #   :bucket            => S3 bucket for upload
-#   :redirect_url      => URL to redirect to after successful upload
+#   :redirect_url      => Base URL to redirect to after successful upload
 #
 # These values can be defined top-level or scoped to the runtime
 # environment (RACK_ENV) much like a Rails' database.yml file
@@ -20,14 +20,25 @@ $config = YAML.load(ERB.new(File.read('config.yml')).result)
 $config = $config[ENV['RACK_ENV']] if $config.has_key? ENV['RACK_ENV']
 
 get '/' do
-  @params = params
-  @policy = policy
-  @bucket = $config[:bucket]
-  @redirect_url = $config[:redirect_url]
-  @encoded_policy = Base64.encode64(@policy).gsub("\n", '')
-  @encoded_signature = Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), $config[:secret_access_key], @encoded_policy)).chomp
-  @aws_access_key_id = $config[:access_key_id]
   erb :index
+end
+
+get '/new' do
+  @aws_access_key_id = $config[:access_key_id]
+  @bucket            = $config[:bucket]
+  @redirect_url      = "#{$config[:redirect_url]}/complete"
+  @policy            = policy(@redirect_url)
+  @signature         = Base64.encode64(OpenSSL::HMAC.digest(
+                         OpenSSL::Digest::Digest.new('sha1'),
+                         $config[:secret_access_key],
+                         @policy
+                       )).chomp
+  
+  erb :new, :layout => false
+end
+
+get '/complete' do
+  "Upload complete!"
 end
 
 helpers do
@@ -35,17 +46,17 @@ helpers do
   alias_method :h, :escape_html
 end
 
-def policy
-  %Q{
+def policy(redirect_url)
+  Base64.encode64(%Q{
 { "expiration": "#{1.day.from_now.strftime('%Y-%m-%dT%H:%M:%SZ')}",
   "conditions": [
     {"bucket": "#{$config[:bucket]}"},
     ["starts-with", "$key", ""],
     {"acl": "private"},
-    {"success_action_redirect": "#{$config[:redirect_url]}"},
+    {"success_action_redirect": "#{redirect_url}"},
     ["starts-with", "$Content-Type", ""],
     ["content-length-range", 0, #{$config[:max_file_size]}]
   ]
 }
-  }
+  }).gsub("\n", '')
 end
--- public/application.js
+++ public/application.js
@@ -1,8 +1,19 @@
 $(function() {
+  $('.upload').load("/new");
+  
+  $('#add').click(function() {
+    div = $('<div class="upload"></div>');
+    div.insertAfter('.upload:last').load("/new");
+    return(false);
+  });
+  
   $('#upload_all').click(function() {
-    var forms = $('form');
-    $.each(forms, function(n, form) {
-      $(this).ajaxSubmit();
+    $.each($('form'), function(n, form) {
+      $(this).ajaxSubmit({
+        success: function(body, status, set) {
+          set.html('<p class="success">Upload complete</p>');
+        }
+      });
     });
   });
 });
